eae714a857b6254ba7a33330860de126acd9c51f
LiesnikovDenys <liesnikov.denys@gmail.com>
2025-03-09 18:48:31 +0200

    Add files via upload

---
+++ LabProjectContext.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using ShopDomain.Model;
+using Microsoft.EntityFrameworkCore;
+
+//namespace ShopDomain.Model;
+namespace ShopInfrastructure;
+
+public partial class LabProjectContext : DbContext
+{
+    public LabProjectContext()
+    {
+    }
+
+    public LabProjectContext(DbContextOptions<LabProjectContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Artist> Artists { get; set; }
+
+    public virtual DbSet<Brand> Brands { get; set; }
+
+    public virtual DbSet<Client> Clients { get; set; }
+
+    public virtual DbSet<Genre> Genres { get; set; }
+
+    public virtual DbSet<Order> Orders { get; set; }
+
+    public virtual DbSet<OrderItem> OrderItems { get; set; }
+
+    public virtual DbSet<OrderStatus> OrderStatuses { get; set; }
+
+    public virtual DbSet<Payment> Payments { get; set; }
+
+    public virtual DbSet<Player> Players { get; set; }
+
+    public virtual DbSet<Vinyl> Vinyls { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-GPDS40E\\SQLEXPRESS; Database=lab_project; Trusted_Connection=True; TrustServerCertificate=True; ");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.UseCollation("Cyrillic_General_CI_AS");
+
+        modelBuilder.Entity<Artist>(entity =>
+        {
+            entity.HasKey(e => e.ArtistId).HasName("PK_Table_8");
+
+            entity.Property(e => e.ArtistId)
+                .ValueGeneratedNever()
+                .HasColumnName("artist_id");
+            entity.Property(e => e.ArtistDescription)
+                .HasMaxLength(10)
+                .IsFixedLength()
+                .HasColumnName("artist_description");
+            entity.Property(e => e.ArtistName)
+                .HasMaxLength(50)
+                .HasColumnName("artist_name");
+        });
+
+        modelBuilder.Entity<Brand>(entity =>
+        {
+            entity.HasKey(e => e.BrandId).HasName("PK_Table_10");
+
+            entity.Property(e => e.BrandId)
+                .ValueGeneratedNever()
+                .HasColumnName("brand_id");
+            entity.Property(e => e.BrandDescription).HasColumnName("brand_description");
+            entity.Property(e => e.BrandName)
+                .HasMaxLength(50)
+                .HasColumnName("brand_name");
+        });
+
+        modelBuilder.Entity<Client>(entity =>
+        {
+            entity.HasKey(e => e.ClientId).HasName("PK_Table_4");
+
+            entity.Property(e => e.ClientId)
+                .ValueGeneratedNever()
+                .HasColumnName("client_id");
+            entity.Property(e => e.Admin)
+                .HasMaxLength(10)
+                .IsFixedLength()
+                .HasColumnName("admin");
+            entity.Property(e => e.Email)
+                .HasMaxLength(50)
+                .HasColumnName("email");
+            entity.Property(e => e.Password)
+                .HasMaxLength(50)
+                .HasColumnName("password");
+            entity.Property(e => e.Phonenumber)
+                .HasMaxLength(50)
+                .HasColumnName("phonenumber");
+            entity.Property(e => e.Username)
+                .HasMaxLength(50)
+                .HasColumnName("username");
+        });
+
+        modelBuilder.Entity<Genre>(entity =>
+        {
+            entity.HasKey(e => e.GenreId).HasName("PK_Table_9");
+
+            entity.Property(e => e.GenreId)
+                .ValueGeneratedNever()
+                .HasColumnName("genre_id");
+            entity.Property(e => e.GenreDescription).HasColumnName("genre_description");
+            entity.Property(e => e.GenreName)
+                .HasMaxLength(50)
+                .HasColumnName("genre_name");
+        });
+
+        modelBuilder.Entity<Order>(entity =>
+        {
+            entity.Property(e => e.OrderId)
+                .ValueGeneratedNever()
+                .HasColumnName("order_id");
+            entity.Property(e => e.ClientId).HasColumnName("client_id");
+            entity.Property(e => e.OrderItemId).HasColumnName("order_item_id");
+            entity.Property(e => e.OrderPrice)
+                .HasColumnType("decimal(10, 2)")
+                .HasColumnName("order_price");
+            entity.Property(e => e.OrderStatusId).HasColumnName("order_status_id");
+            entity.Property(e => e.PaymentId).HasColumnName("payment_id");
+
+            entity.HasOne(d => d.Client).WithMany(p => p.Orders)
+                .HasForeignKey(d => d.ClientId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Orders_Clients");
+
+            entity.HasOne(d => d.OrderItem).WithMany(p => p.Orders)
+                .HasForeignKey(d => d.OrderItemId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Orders_Table_5");
+
+            entity.HasOne(d => d.OrderStatus).WithMany(p => p.Orders)
+                .HasForeignKey(d => d.OrderStatusId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Orders_Table_2");
+
+            entity.HasOne(d => d.Payment).WithMany(p => p.Orders)
+                .HasForeignKey(d => d.PaymentId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Orders_Table_3");
+        });
+
+        modelBuilder.Entity<OrderItem>(entity =>
+        {
+            entity.HasKey(e => e.OrderItemId).HasName("PK_Table_5");
+
+            entity.ToTable("Order items");
+
+            entity.Property(e => e.OrderItemId)
+                .ValueGeneratedNever()
+                .HasColumnName("order_item_id");
+            entity.Property(e => e.PlayerId).HasColumnName("player_id");
+            entity.Property(e => e.VinilId).HasColumnName("vinil_id");
+
+            entity.HasOne(d => d.Player).WithMany(p => p.OrderItems)
+                .HasForeignKey(d => d.PlayerId)
+                .HasConstraintName("FK_Order items_Players");
+
+            entity.HasOne(d => d.Vinil).WithMany(p => p.OrderItems)
+                .HasForeignKey(d => d.VinilId)
+                .HasConstraintName("FK_Order items_Vinyls");
+        });
+
+        modelBuilder.Entity<OrderStatus>(entity =>
+        {
+            entity.HasKey(e => e.OrderStatusId).HasName("PK_Table_2");
+
+            entity.ToTable("Order statuses");
+
+            entity.Property(e => e.OrderStatusId)
+                .ValueGeneratedNever()
+                .HasColumnName("order_status_id");
+            entity.Property(e => e.OrderStatus1)
+                .HasMaxLength(50)
+                .HasColumnName("order_status");
+        });
+
+        modelBuilder.Entity<Payment>(entity =>
+        {
+            entity.HasKey(e => e.PaymentId).HasName("PK_Table_3");
+
+            entity.Property(e => e.PaymentId)
+                .ValueGeneratedNever()
+                .HasColumnName("payment_id");
+            entity.Property(e => e.PaymentStatus).HasColumnName("payment_status");
+            entity.Property(e => e.PaymentType)
+                .HasMaxLength(50)
+                .HasColumnName("payment_type");
+        });
+
+        modelBuilder.Entity<Player>(entity =>
+        {
+            entity.HasKey(e => e.PlayerId).HasName("PK_Table_7");
+
+            entity.Property(e => e.PlayerId)
+                .ValueGeneratedNever()
+                .HasColumnName("player_id");
+            entity.Property(e => e.Bluetooth).HasColumnName("bluetooth");
+            entity.Property(e => e.BrandId).HasColumnName("brand_id");
+            entity.Property(e => e.Price)
+                .HasMaxLength(10)
+                .IsFixedLength()
+                .HasColumnName("price");
+            entity.Property(e => e.Speaker).HasColumnName("speaker");
+            entity.Property(e => e.Stock).HasColumnName("stock");
+
+            entity.HasOne(d => d.Brand).WithMany(p => p.Players)
+                .HasForeignKey(d => d.BrandId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Table_7_Table_10");
+        });
+
+        modelBuilder.Entity<Vinyl>(entity =>
+        {
+            entity.HasKey(e => e.VinilId).HasName("PK_Table_6");
+
+            entity.Property(e => e.VinilId)
+                .ValueGeneratedNever()
+                .HasColumnName("vinil_id");
+            entity.Property(e => e.ArtistId).HasColumnName("artist_id");
+            entity.Property(e => e.GenreId).HasColumnName("genre_id");
+            entity.Property(e => e.Price)
+                .HasMaxLength(10)
+                .IsFixedLength()
+                .HasColumnName("price");
+            entity.Property(e => e.Stock).HasColumnName("stock");
+            entity.Property(e => e.VinilName)
+                .HasMaxLength(50)
+                .HasColumnName("vinil_name");
+
+            entity.HasOne(d => d.Artist).WithMany(p => p.Vinyls)
+                .HasForeignKey(d => d.ArtistId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Table_6_Table_8");
+
+            entity.HasOne(d => d.Genre).WithMany(p => p.Vinyls)
+                .HasForeignKey(d => d.GenreId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Table_6_Table_9");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
